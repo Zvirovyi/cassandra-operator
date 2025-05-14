7a3138b7a43b6dd070d4cd380ba72b2c05277134
Vladyslav Yurchenko <silvia.homam@gmail.com>
2025-05-14 19:38:42 +0300

    Start

--- src/charm.py
+++ src/charm.py
@@ -7,6 +7,7 @@
 import logging
 
 import ops
+import os 
 
 logger = logging.getLogger(__name__)
 
@@ -17,11 +18,39 @@ class CassandraOperatorCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on.start, self._on_start)
+        framework.observe(self.on.install, self._on_install)
 
     def _on_start(self, event: ops.StartEvent):
         """Handle start event."""
         self.unit.status = ops.ActiveStatus()
+        
+    def _on_install(self, event: ops.InstallEvent):
+        """Handle install event."""
+        
+        self.unit.status = ops.MaintenanceStatus("Installing cassandra snap")
+        logger.debug(f"---------- INSTALLING SNAP ----------")
+        os.system(f"snap install cassandra_5.0.4_amd64.snap --devmode --dangerous")
+        logger.debug(f"---------- SNAP INSTALLED ----------")
+        self.unit.status = ops.ActiveStatus("Ready")
 
+    def _getWorkloadVersion(self):
+        """Get the microsample workload version from the snapd API via unix-socket"""
+        snap_name = "cassandra"
+        snapd_url = f"http+unix://%2Frun%2Fsnapd.socket/v2/snaps/{snap_name}"
+        session = requests_unixsocket.Session()
+        # Use the requests library to send a GET request over the Unix domain socket
+        response = session.get(snapd_url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            logger.debug(f"---------- SOCKET RESONSE: {data} ----------")
+            workload_version = data["result"]["version"]
+        else:
+            workload_version = "unknown"
+            print(f"Failed to retrieve Snap apps. Status code: {response.status_code}")
 
+        # Return the workload version
+        return workload_version
+        
 if __name__ == "__main__":  # pragma: nocover
     ops.main(CassandraOperatorCharm)
