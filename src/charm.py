8dbb43c6602cb765ab06e5fad809f8ae6cc6ffb9
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2025-05-19 19:59:57 +0300

    Service startup. Refactor.

--- src/charm.py
+++ src/charm.py
@@ -7,59 +7,72 @@
 import logging
 import os
 
-import ops
 import requests_unixsocket
+from ops import (
+    ActiveStatus,
+    CharmBase,
+    Framework,
+    InstallEvent,
+    MaintenanceStatus,
+    StartEvent,
+    main,
+)
 
 logger = logging.getLogger(__name__)
 
 
-class CassandraOperatorCharm(ops.CharmBase):
+class CassandraOperatorCharm(CharmBase):
     """Charm the application."""
 
-    def __init__(self, framework: ops.Framework):
+    def __init__(self, framework: Framework):
         super().__init__(framework)
         framework.observe(self.on.start, self._on_start)
         framework.observe(self.on.install, self._on_install)
 
-    def _on_start(self, event: ops.StartEvent):
+    def _on_start(self, event: StartEvent) -> None:
         """Handle start event."""
-        self.unit.status = ops.ActiveStatus()
+        self.unit.set_workload_version(self._get_workload_version())
+        # TODO: get rid of os.system hack
+        os.system("snap connect cassandra:log-observe")
+        os.system("snap connect cassandra:mount-observe")
+        os.system("snap connect cassandra:process-control")
+        os.system("snap connect cassandra:system-observe")
+        os.system("snap connect cassandra:sys-fs-cgroup-service")
+        os.system("snap connect cassandra:shmem-perf-analyzer")
+        os.system("snap start cassandra")
+        self._set_unit_status()
 
+    def _on_install(self, event: InstallEvent) -> None:
+        """Handle install event."""
+        self.unit.status = MaintenanceStatus("Installing cassandra snap")
+        logger.debug("Installing snap")
+        # TODO: use charms.operator_libs_linux.v2.snap and catch errors
+        os.system("snap install cassandra_5.0.4_amd64.snap --devmode --dangerous")
+        logger.debug("Snap successfully installed")
+        self._set_unit_status()
+
+    def _set_unit_status(self) -> None:
+        self.unit.status = ActiveStatus()
+
+    def _get_workload_version(self) -> str:
+        """Get the microsample workload version from the snapd API via unix-socket."""
         try:
-            version = self._get_workload_version()
-            self.unit.set_workload_version(version)
+            snap_name = "cassandra"
+            snapd_url = f"http+unix://%2Frun%2Fsnapd.socket/v2/snaps/{snap_name}"
+            session = requests_unixsocket.Session()
+            response = session.get(snapd_url)
+            if response.status_code == 200:
+                data = response.json()
+                return data["result"]["version"]
+            else:
+                logger.error(
+                    f"Failed to retrieve snap apps for workload version. Status code: {response.status_code}"
+                )
+                return "unknown"
         except Exception as e:
             logger.error(f"Failed to get workload version: {e}")
-            self.unit.set_workload_version("unknown")
-
-    def _on_install(self, event: ops.InstallEvent):
-        """Handle install event."""
-        self.unit.status = ops.MaintenanceStatus("Installing cassandra snap")
-        logger.debug("---------- INSTALLING SNAP ----------")
-        os.system("snap install cassandra_5.0.4_amd64.snap --devmode --dangerous")
-        logger.debug("---------- SNAP INSTALLED ----------")
-        self.unit.status = ops.ActiveStatus("Ready")
-
-    def _get_workload_version(self):
-        """Get the microsample workload version from the snapd API via unix-socket."""
-        logger.debug("---------- GETTING WORKLOAD VERSION ----------")
-        snap_name = "cassandra"
-        snapd_url = f"http+unix://%2Frun%2Fsnapd.socket/v2/snaps/{snap_name}"
-        session = requests_unixsocket.Session()
-        # Use the requests library to send a GET request over the Unix domain socket
-        response = session.get(snapd_url)
-        # Check if the request was successful
-        if response.status_code == 200:
-            data = response.json()
-            logger.debug(f"---------- SOCKET RESONSE: {data} ----------")
-            workload_version = data["result"]["version"]
-        else:
-            workload_version = "unknown"
-            print(f"Failed to retrieve Snap apps. Status code: {response.status_code}")
-
-        # Return the workload version
-        return workload_version
+            return "unknown"
 
 
 if __name__ == "__main__":  # pragma: nocover
-    ops.main(CassandraOperatorCharm)
+    main(CassandraOperatorCharm)
