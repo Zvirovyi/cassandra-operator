5287ed8a67d36b97ca29668520408569fc6a43b2
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2025-05-20 12:44:20 +0300

    Improve workload version logic.

--- src/charm.py
+++ src/charm.py
@@ -5,9 +5,7 @@
 """Charm the application."""
 
 import logging
-import os
 
-import requests_unixsocket
 from charms.operator_libs_linux.v2 import snap
 from ops import (
     ActiveStatus,
@@ -31,13 +29,11 @@ class CassandraOperatorCharm(CharmBase):
         framework.observe(self.on.install, self._on_install)
 
     def _on_start(self, event: StartEvent) -> None:
-        """Handle start event."""
-        self.unit.set_workload_version(self._get_workload_version())
-        self._cassandra_snap().start(enable=True)
+        self._set_unit_workload_version()
+        self._cassandra_snap().start(["daemon"], True)
         self._set_unit_status()
 
     def _on_install(self, event: InstallEvent) -> None:
-        """Handle install event."""
         self.unit.status = MaintenanceStatus("Installing cassandra snap")
         logger.debug("Installing snap")
         snap.install_local("cassandra_5.0.4_amd64.snap", devmode=True, dangerous=True)
@@ -57,24 +53,12 @@ class CassandraOperatorCharm(CharmBase):
     def _cassandra_snap(self) -> snap.Snap:
         return snap.SnapCache()["cassandra"]
 
-    def _get_workload_version(self) -> str:
-        """Get the microsample workload version from the snapd API via unix-socket."""
-        try:
-            snap_name = "cassandra"
-            snapd_url = f"http+unix://%2Frun%2Fsnapd.socket/v2/snaps/{snap_name}"
-            session = requests_unixsocket.Session()
-            response = session.get(snapd_url)
-            if response.status_code == 200:
-                data = response.json()
-                return data["result"]["version"]
-            else:
-                logger.error(
-                    f"Failed to retrieve snap apps for workload version. Status code: {response.status_code}"
-                )
-                return "unknown"
-        except Exception as e:
-            logger.error(f"Failed to get workload version: {e}")
-            return "unknown"
+    def _set_unit_workload_version(self) -> None:
+        client = snap.SnapClient()
+        for snp in client.get_installed_snaps():
+            if snp["name"] == "cassandra":
+                self.unit.set_workload_version(str(snp["version"]))
+                return
 
 
 if __name__ == "__main__":  # pragma: nocover
